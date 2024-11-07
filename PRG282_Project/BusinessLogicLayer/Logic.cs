e763c65d43c5d5f824f6467155232c74809a8594
Ugochukwu Winner Ogbonnaya <600528@student.belgiumcampus.ac.za>
2024-11-08 00:47:13 +0200

    Updated Calculate method, to return int and double values
    Created Add, Update, Search(Search by Student ID, Name, Age and Course)
    
    Created Input Validation methods, using Regular Expressions (Regex)

--- PRG282_Project/BusinessLogicLayer/Logic.cs
+++ PRG282_Project/BusinessLogicLayer/Logic.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data;
+using System.Text.RegularExpressions;
 
 // import namespaces
 using PRG282_Project.DataAccessLayer;
@@ -16,9 +17,8 @@ namespace PRG282_Project.BusinessLogicLayer
         FileHandler fh = new FileHandler(); // Object of the FileHandler class
 
         // Method to calculate for a summary report
-        public void Calculate()
+        public (int, double) Calculate()
         {
-            
             List<Student> students = fh.Read(); // Reads from the orignal list
             int totalAge = 0;
 
@@ -29,10 +29,12 @@ namespace PRG282_Project.BusinessLogicLayer
             }
 
             int totalStudents = students.Count; // Counts total sstudents
-            double averageAge = totalAge / totalStudents; // Calculates total average using total student
+            double averageAge = Math.Round((Convert.ToDouble(totalAge) / totalStudents), 2); // Calculates total average using total student and rounds answer to two decimal places
 
             // Pass calculated values to the GenerateSummary method
             fh.GenerateSummary(totalStudents, averageAge);
+
+            return (totalStudents, averageAge);
         }
         public void DeleteStudent(string studentId)
         {
@@ -48,6 +50,7 @@ namespace PRG282_Project.BusinessLogicLayer
             }
 
             fh.Write(students); // Passes the updated list to user
+            MessageBox.Show("Student was deleted successfully!");
         }
 
         public DataTable DisplayStudents()
@@ -67,6 +70,212 @@ namespace PRG282_Project.BusinessLogicLayer
             }
 
             return dt;
+        }
+
+        public void AddStudent(string studentId, string name, int age, string course)
+        {
+            List<Student> students = fh.Read(); // Reads from original list
+            {
+                new Student(studentId, name, age, course); // Adding a new Student to list
+            }
+
+            fh.Write(students); // Passing updated student list to FileHandler
+        }
+        public void UpdateStudent(string studentID, string studentName, int age, string course)
+        {
+            List<Student> students = fh.Read();
+            foreach (Student student in students)
+            {
+                if (student.StudentId == studentID)
+                {
+                    // Overriding student details if the student ID matches
+                    student.Name = studentName;
+                    student.Age = age;
+                    student.Course = course;
+                }
+            }
+
+            fh.Write(students); // Passing updated student list to FileHandler
+        }
+
+        // Search by Student ID
+        public DataTable SearchStudentID(string studentId)
+        {
+            DataTable dt = new DataTable();
+            List<Student> students = fh.Read(); // Reads from original list
+
+            // Define columns for the DataTable
+            dt.Columns.Add("StudentId");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Age");
+            dt.Columns.Add("Course");
+
+            foreach (var student in students)
+            {
+                if (student.StudentId == studentId)
+                {
+                    dt.Rows.Add(student); // Add each student's data as a new row in the DataTable
+                    return dt; // Returns immediately after finding matching student ID
+                }
+            }
+            MessageBox.Show($"Student ID '{studentId}' not found!");
+
+            return dt;
+        }
+
+        // Search by Name
+        public DataTable SearchStudentName(string name)
+        {
+            DataTable dt = new DataTable();
+            List<Student> students = fh.Read(); // Reads from original list
+
+            // Define columns for the DataTable
+            dt.Columns.Add("StudentId");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Age");
+            dt.Columns.Add("Course");
+
+            bool nameFound = false;
+
+            foreach (var student in students)
+            {
+                if (student.Name == name)
+                {
+                    nameFound = true;
+                    dt.Rows.Add(student); // Add each student's data as a new row in the DataTable
+                }
+            }
+            if (!nameFound)
+            {
+                MessageBox.Show($"Name '{name}' not found!");
+            }
+
+            return dt;
+        }
+
+
+        // Search by Age
+        public DataTable SearchStudentAge(int age)
+        {
+            DataTable dt = new DataTable();
+            List<Student> students = fh.Read(); // Reads from original list
+
+            // Define columns for the DataTable
+            dt.Columns.Add("StudentId");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Age");
+            dt.Columns.Add("Course");
+
+            bool ageFound = false;
+
+            foreach (var student in students)
+            {
+                if (student.Age == age)
+                {
+                    ageFound = true;
+                    dt.Rows.Add(student); // Add each student's data as a new row in the DataTable
+                }
+            }
+            if (!ageFound)
+            {
+                MessageBox.Show($"Age '{age}' not found!");
+            }
+
+            return dt;
+        }
+
+        // Search by Course
+        public DataTable SearchStudentCourse(string course)
+        {
+            DataTable dt = new DataTable();
+            List<Student> students = fh.Read(); // Reads from original list
+
+            // Define columns for the DataTable
+            dt.Columns.Add("StudentId");
+            dt.Columns.Add("Name");
+            dt.Columns.Add("Age");
+            dt.Columns.Add("Course");
+
+            bool courseFound = false;
+
+            foreach (var student in students)
+            {
+                if (student.Course == course)
+                {
+                    courseFound = true;
+                    dt.Rows.Add(student); // Add each student's data as a new row in the DataTable
+                }
+            }
+
+            if(!courseFound)
+            {
+                MessageBox.Show($"Course '{course}' not found!");
+            }
+
+            return dt;
+        }
+
+
+        // Input Validations
+        string pattern = ""; // Declared pattern as a global variable
+        public Boolean ValidID(string studentId)
+        {
+            pattern = @"^\d{6}$"; // Validates student IDs with 6 digit numbers (no letters or other special characters)
+
+            Regex rg = new Regex(pattern);
+
+            bool validID = rg.IsMatch(studentId); // Stores true if the Student Id is valid, else false
+
+            if (validID)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean ValidName(string name)
+        {
+            pattern = @"^[a-zA-Z]{3,}$"; // Validates name with 3 minimum characters (letters only and no spaces or other characters)
+
+            Regex rg = new Regex(pattern);
+
+            bool validName = rg.IsMatch(name); // Stores true if the Name is valid, else false
+
+            if (validName)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean ValidAge(int age)
+        {
+            pattern = @"^[1-9]\d$"; // Validates age between 10 and 99
+
+            Regex rg = new Regex(pattern);
+
+            bool validAge = rg.IsMatch(age.ToString()); // Stores true if the Age is valid, else false
+
+            if (validAge)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public Boolean ValidCourse(string course)
+        {
+            pattern = @"^[a-zA-Z]{3,10}$"; // Validates course name between 3 to 10 characters (letters only and no spaces or other characters)
+
+            Regex rg = new Regex(pattern);
+
+            bool validCourse = rg.IsMatch(course); // Stores true if the Course name is valid, else false
+
+            if (validCourse)
+            {
+                return true;
+            }
+            return false;
         }
     }
 }
--- PRG282_Project/DataAccessLayer/FileHandler.cs
+++ PRG282_Project/DataAccessLayer/FileHandler.cs
@@ -4,15 +4,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System.Windows.Forms;
 
 namespace PRG282_Project.DataAccessLayer
 {
     internal class FileHandler
     {
-        
-
+        // File paths
         string path = @"students.txt";
         string summaryPath = @"summary.txt";
+
+        // Reading data from the student.txt file
         public List<Student> Read()
         {
             List<Student> students = new List<Student>();
@@ -27,38 +29,38 @@ namespace PRG282_Project.DataAccessLayer
                         Student student = new Student(templines[0], templines[1], int.Parse(templines[2]), templines[3]);
                         students.Add(student);
                     }
-     
                 }
-                
             }
             else
             {
-                Console.WriteLine("File not found!");
+                MessageBox.Show("File not found!");
             }
-            return students;
 
+            return students;
         }
+
+        // Updating the student.txt file by overriding it
         public void Write(List<Student> students)
         {
-
             using(StreamWriter sw = new StreamWriter(path))
             {
                 foreach (Student student in students)
                 {
+                    // Writing to the file
                     sw.WriteLine($"{student.StudentId},{student.Name},{student.Age},{student.Course}");
                 }
-            }                    
-            Console.WriteLine("Formatted and written to file");
-
+            }
+            MessageBox.Show("student.txt file was updated successfully, please check inside the bin folder");
         }
 
         public void GenerateSummary(int totalStudents, double averageAge)
         {
             using (StreamWriter sw = new StreamWriter(summaryPath))
             {
+                // Writing the summary to the summary.txt file
                 sw.WriteLine($"Total Students: {totalStudents}\nAverage Age: {averageAge}");
             }
-            Console.WriteLine("Summary Report written to file");
+            MessageBox.Show("Summary Report written to summary.txt file");
         }
     }
 }
