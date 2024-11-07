5e18c97d840f26bbac0e93927b73c8bfc0d6ad11
KeitumetseSetati <143459560+KeitumetseSetati@users.noreply.github.com>
2024-11-07 05:21:17 +0200

    Update FileHandler.cs

--- PRG282_Project/DataAccessLayer/FileHandler.cs
+++ PRG282_Project/DataAccessLayer/FileHandler.cs
@@ -14,5 +14,43 @@ namespace PRG282_Project.DataAccessLayer
         // 1. Writing and Reading text file (Create, Read, Update, Delete)
         // 2. Search students
         // 3. Generate summary (from the logic layer)
+
+         public void write(List<Students> plist)
+        {
+
+            
+
+            List<string> output = new List<string>();
+            foreach (Students person in plist)
+            {
+                output.Add(person.ToString());
+            }
+            File.WriteAllLines(path, output);
+            Console.WriteLine("Formatted and written to file");
+
+        }
+
+        
+
+        public List<string> read()
+        {
+            List<string> list = new List<string>();
+            if (File.Exists(path))
+            {
+                
+                list = File.ReadAllLines(path).ToList();
+
+                Console.WriteLine("Original Data:");
+                Console.WriteLine("======================");
+
+                foreach (string line in list)
+                {
+                    Console.WriteLine(line);
+                }
+                Console.WriteLine("=========================");
+            }
+            
+            return list;
+        }
     }
 }
