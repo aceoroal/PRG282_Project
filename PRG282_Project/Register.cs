ec9ac3a3e7835ca7ad829843752accf97bbb563d
aceoroal <aceoroalcompany03@gmail.com>
2024-11-09 23:48:18 +0200

    Fixed the Button click and Form Load event handlers in the Register Form.

--- PRG282_Project/Register.cs
+++ PRG282_Project/Register.cs
@@ -8,6 +8,10 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+// import namespaces
+using PRG282_Project.BusinessLogicLayer;
+using PRG282_Project.PresentationLayer;
+
 namespace PRG282_Project
 {
     public partial class Register : Form
@@ -15,6 +19,47 @@ namespace PRG282_Project
         public Register()
         {
             InitializeComponent();
+        }
+
+        public void ClearFields()
+        {
+            txtStudentID.Clear();
+            txtName.Clear();
+            txtAge.Value = 1;
+            cboCourse.SelectedIndex = -1;
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            if (cboCourse.Text != "")
+            {
+                string studentId = txtStudentID.Text;
+                string name = txtName.Text;
+                int age = Convert.ToInt32(txtAge.Value);
+                string course = cboCourse.Text;
+
+                UserInput input = new UserInput();
+                if (input.AddStudent(studentId, name, age, course))
+                {
+                    // Clear all fields
+                    ClearFields();
+
+                    Logic logic = new Logic();
+                    logic.PassGeneratedStudentID();
+                    txtStudentID.Text = logic.GenerateStudentID();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please choose a course!");
+                cboCourse.Focus();
+            }
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+            Logic logic = new Logic();
+            txtStudentID.Text = logic.GenerateStudentID();
         }
     }
 }
